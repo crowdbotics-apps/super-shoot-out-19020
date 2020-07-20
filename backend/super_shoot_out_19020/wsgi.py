d9133b5130b3c4ec50645312a61e41bddce43287
Crowdbotics <team@crowdbotics.com>
2020-07-20 01:39:11 +0000

    Update models

--- backend/super_shoot_out_19020/wsgi.py
+++ backend/super_shoot_out_19020/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'super_shoot_out_19020.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "super_shoot_out_19020.settings")
 
 application = get_wsgi_application()
