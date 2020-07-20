a855d399e0e006ca1c9e5c14298c899bb6950864
Crowdbotics <team@crowdbotics.com>
2020-07-20 01:32:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/super_shoot_out_19020/MainActivity.java
@@ -0,0 +1,15 @@
+package com.super_shoot_out_19020;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "super_shoot_out_19020";
+    }
+}
